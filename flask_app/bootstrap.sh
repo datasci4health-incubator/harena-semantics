9522b10e20de0f2c6641d710d3ed762e716a9fd9
faguim <pantoja.ti@gmail.com>
2019-07-21 22:18:04 -0300

    Instaling spacy package

--- flask_app/bootstrap.sh
+++ flask_app/bootstrap.sh
@@ -2,4 +2,8 @@
 export FLASK_APP=./src/index.py
 export FLASK_ENV=development
 source $(pipenv --venv)/bin/activate
+
+#pip install spacy
+python -m spacy download en_core_web_sm
+
 flask run -h 0.0.0.0
