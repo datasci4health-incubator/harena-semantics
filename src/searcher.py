2b3621a131d03fd5874c3a062f45451ea50d123d
faguim <pantoja.ti@gmail.com>
2019-08-05 23:29:59 -0300

    Start coding seachByCategory method

--- src/searcher.py
+++ src/searcher.py
@@ -4,22 +4,42 @@ from model.topic import Topic
 
 TOPICS_FILE_LOCATION = '../resources/topics2014.xml'
 
-tree = et.parse(TOPICS_FILE_LOCATION)
+filter = 'randomized controlled trial[Publication Type] OR randomized[Title/Abstract] OR placebo[Title/Abstract]'
 
-topics_et = tree.getroot()
-topics = []
+print(filter)
 
-for topic_et in topics_et:
+def get_topics():
+    tree = et.parse(TOPICS_FILE_LOCATION)
 
-    type = topic_et.attrib['type']
-    number = topic_et.attrib['number']
-    description = topic_et.find("description").text
-    summary = topic_et.find("summary").text
+    topics_et = tree.getroot()
+    topics = []
 
-    topic = Topic(description, summary, number, type)
+    for topic_et in topics_et:
+        type = topic_et.attrib['type']
+        number = topic_et.attrib['number']
+        description = topic_et.find("description").text
+        summary = topic_et.find("summary").text
 
-    # print(topic)
-    topic = json.dumps(topic.__dict__)
-    topics.append(topic)
+        topic = Topic(description, summary, number, type)
 
-print(topics)
+        # print(topic)
+        topic = json.dumps(topic.__dict__)
+        topics.append(topic)
+    return topics
+
+def filterPapers():
+
+
+def searchByCategory(topicDescription, topicNumber):
+
+
+def experiment(topics):
+    for topic in topics:
+        searchByCategory(topic.description, topic.number)
+
+
+
+
+topics = get_topics()
+#
+# print(topics)
