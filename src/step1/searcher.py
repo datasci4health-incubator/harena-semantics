264d63e871b542eb86ed1748be8e61240ceb0642
faguim <pantoja.ti@gmail.com>
2019-08-12 22:25:14 -0300

    Search by topic category

---
+++ src/step1/searcher.py
@@ -0,0 +1,64 @@
+import xml.etree.ElementTree as et
+import pysolr, json, os
+
+from model.topic import Topic
+
+TOPICS_FILE_LOCATION = '../resources/topics2014.xml'
+
+SOLR_URL = 'http://' + os.environ['SOLR_HOST'] + ':8983/solr/pmc3'
+
+
+def get_topics():
+    tree = et.parse(TOPICS_FILE_LOCATION)
+
+    topics_et = tree.getroot()
+    topics = []
+
+    for topic_et in topics_et:
+        type = topic_et.attrib['type']
+        number = topic_et.attrib['number']
+        description = topic_et.find("description").text
+        description = description.replace(':', '\:')
+
+        summary = topic_et.find("summary").text
+
+        topic = Topic(description, summary, number, type)
+
+        topics.append(topic)
+    return topics
+
+
+def search_by_category(topic_description, filter):
+    solr = pysolr.Solr(SOLR_URL)
+
+    retrievedDocs = solr.search('body:'+topic_description, fq=filter)
+    pmcs = []
+    for doc in retrievedDocs:
+        pmcs.append(doc['pmc'])
+    return pmcs
+
+
+topics = get_topics()
+
+results = []
+
+with open('filters.json') as f:
+    filters = json.load(f)
+
+print(filters)
+
+for topic in topics:
+    result = dict()
+
+    filter = filters.get(topic.type)
+    if filter is not None:
+        pmcs = search_by_category(topic.description, filter)
+        result.update({ 'topic':topic.number, 'pmc':pmcs })
+        results.append(result)
+print(results)
+
+if not os.path.exists('../result'):
+    os.makedirs('../result')
+
+with open('../result/step1.json', 'w') as outfile:
+    json.dump(results, outfile)
