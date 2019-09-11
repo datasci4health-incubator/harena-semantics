8e071dc47073203169e226b4352fdd05f74a0be1
faguim <pantoja.ti@gmail.com>
2019-09-11 16:02:38 -0300

    Search by mesh terms

--- src/step1/searcher.py
+++ src/step1/searcher.py
@@ -4,12 +4,12 @@ from ncbo.ncbo_annotator import get_mesh_terms
 
 from model.topic import Topic
 
+SOLR_URL = 'http://' + os.environ['SOLR_HOST'] + ':8983/solr/pmc'
 TOPICS_FILE_LOCATION = './step1/resources/topics2014.xml'
 
 if not os.path.exists('./step1/results'):
     os.makedirs('./step1/results')
 
-SOLR_URL = 'http://' + os.environ['SOLR_HOST'] + ':8983/solr/pmc'
 
 def get_topics():
     tree = et.parse(TOPICS_FILE_LOCATION)
@@ -31,28 +31,30 @@ def get_topics():
     return topics
 
 
-def search_by_category(topic_description, filter):
-    solr = pysolr.Solr(SOLR_URL)
-    query = 'abstract:' + topic_description + 'or body:' + topic_description
-    retrievedDocs = solr.search(q=query, fq=filter)
-    pmcs = []
-    for doc in retrievedDocs:
-        pmcs.append(doc['pmc'])
-    return pmcs
-
-
-def search_by_category_mesh(mesh_terms, filter):
-    querySearch = ''
+def get_query_from_mesh_terms(mesh_terms):
+    query_search = ''
 
     for mesh_term in mesh_terms:
-        querySearch = querySearch + mesh_term + ' '
+        aux = mesh_term.split()
+        compound_term = ''
+        if len(aux) > 1:
+            compound_term = '('
 
+            for x in aux:
+                compound_term = compound_term + '+' + x + ' '
+            compound_term = compound_term + ')'
+            query_search = query_search + compound_term + ' '
+        else:
+            query_search = query_search + mesh_term + ' '
+    return query_search
+
+
+def search_by_category(field, query_search, filter):
     solr = pysolr.Solr(SOLR_URL)
-    query = 'abstract:' + querySearch
-    print(query)
-    retrievedDocs = solr.search(q=query, fq=filter)
+    query = field + ':' + query_search
+    retrieved_docs = solr.search(q=query, fq=filter)
     pmcs = []
-    for doc in retrievedDocs:
+    for doc in retrieved_docs:
         pmcs.append(doc['pmc'])
     return pmcs
 
@@ -60,17 +62,28 @@ def search_by_category_mesh(mesh_terms, filter):
 with open('./step1/resources/filters.json') as f:
     filters = json.load(f)
 
-results = []
+mesh_results = []
+description_results = []
 topics = get_topics()
 
 for topic in topics:
     filter = filters.get(topic.type)
     if filter is not None:
-        # pmcs = search_by_category(topic.description, filter)
-        mesh = get_mesh_terms(topic.description)
-        pmcs = search_by_category_mesh(mesh, filter)
-        result = {'topic':topic.number, 'pmc':pmcs}
-        results.append(result)
+        pmcs_by_description = search_by_category('abstract', topic.description, filter)
 
-with open('./step1/results/abstract.json', 'w') as outfile:
-    json.dump(results, outfile)
+        mesh_terms = get_mesh_terms(topic.description)
+        search_query = get_query_from_mesh_terms(mesh_terms)
+        pmcs_by_mesh = search_by_category('abstract', search_query, filter)
+
+
+        description_result = {'topic':topic.number, 'pmc':pmcs_by_description}
+        mesh_result = {'topic':topic.number, 'pmc':pmcs_by_mesh}
+
+        description_results.append(description_result)
+        mesh_results.append(mesh_result)
+
+with open('./step1/results/by_description.json', 'w') as outfile:
+    json.dump(description_results, outfile)
+
+with open('./step1/results/by_mesh.json', 'w') as outfile:
+    json.dump(mesh_results, outfile)
