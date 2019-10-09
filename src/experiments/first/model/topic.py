708a1055c7459a69d92c47e5ef50b96a15b789ed
faguim <pantoja.ti@gmail.com>
2019-10-09 12:13:33 -0300

    Workin on GET /searcher Rest endpoint

--- src/experiments/first/model/topic.py
+++
@@ -1,9 +0,0 @@
-class Topic(object):
-    description = ''
-
-    def __init__(self, description, summary, number, type):
-        self.description = description
-        self.summary = summary
-        self.number = number
-        self.type = type
-
