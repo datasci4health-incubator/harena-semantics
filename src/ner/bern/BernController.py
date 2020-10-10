5cfc6b0304cf627db0829e03ba0d3e47abf1a9ce
Fagner <pantoja.ti@gmail.com>
2020-10-10 19:25:50 -0300

    refactor

--- src/ner/bern/BernController.py
+++ src/ner/bern/BernController.py
@@ -7,38 +7,37 @@ class BernController:
 
     def retrieve_ner(self, text):
         # Uncomment when production env
-        # url='https://bern.korea.ac.kr/plain'
-        # response = requests.post(url, data={'sample_text': text}).json()
+        url='https://bern.korea.ac.kr/plain'
+        response = requests.post(url, data={'sample_text': text}).json()
 
         # when is development env, i'll we a file instead call bern service in order to prevent so many requests
         # comment this when at production
-        with open('src/ner/bern/bern_response_example.json') as json_file:
-            data = json.load(json_file)
-            response = data
+        # with open('src/ner/bern/bern_response_example.json') as json_file:
+        #     data = json.load(json_file)
+        #     response = data
 
-        diseases = response['logits']['disease']
 
+        denotations = response['denotations']
         annotations = []
 
-        # each disease is [ {id, start, end}, float]
-        for disease in diseases:
-            start = disease[0]['start']
-            end = disease[0]['end']
+        for denotation in denotations:
+            obj = denotation['obj']
+            start = denotation['span']['begin']
+            end = denotation['span']['end']
 
-            annotation = '{{ ' + text[start:end] + ' }}'
-
-            context_annotation = (annotation, (start, end))
+            annotation = '{' + text[start:end] +'}('+ obj + ')'
+            extra_chars = len(annotation) - len(text[start:end])
+            context_annotation = (annotation, (start, end, extra_chars))
             annotations.append(context_annotation)
 
         print(annotations)
 
         i = 0
         for annotation in annotations:
-            start = annotation[1][0] + (i * 6)
-            end = annotation[1][1] + (i * 6)
+            start = annotation[1][0] + i
+            end = annotation[1][1] + i
 
             text = text[0:start] + annotation[0] + text[end:]
-
-            i+= 1
+            i+= annotation[1][2]
 
         return text
