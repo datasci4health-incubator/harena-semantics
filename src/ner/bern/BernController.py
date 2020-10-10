0c45d3aedd816e6793ce6b2f0329364268d79667
Fagner <pantoja.ti@gmail.com>
2020-10-10 18:41:21 -0300

    feat(biobert endpoint): `POST /ner` is working

--- src/ner/bern/BernController.py
+++ src/ner/bern/BernController.py
@@ -1,18 +1,44 @@
-import requests
+import requests, json
 
+import re
 
 class BernController:
 
 
     def retrieve_ner(self, text):
-        url='https://bern.korea.ac.kr/plain'
+        # Uncomment when production env
+        # url='https://bern.korea.ac.kr/plain'
+        # response = requests.post(url, data={'sample_text': text}).json()
 
-        response = requests.post(url, data={'sample_text': text}).json()
-        print('logits')
-        logits = response['logits']
+        # when is development env, i'll we a file instead call bern service in order to prevent so many requests
+        # comment this when at production
+        with open('src/ner/bern/bern_response_example.json') as json_file:
+            data = json.load(json_file)
+            response = data
 
-        diseases = logits['disease']
+        diseases = response['logits']['disease']
 
-        print(diseases)
+        annotations = []
 
-        return diseases
+        # each disease is [ {id, start, end}, float]
+        for disease in diseases:
+            start = disease[0]['start']
+            end = disease[0]['end']
+
+            annotation = '{{ ' + text[start:end] + ' }}'
+
+            context_annotation = (annotation, (start, end))
+            annotations.append(context_annotation)
+
+        print(annotations)
+
+        i = 0
+        for annotation in annotations:
+            start = annotation[1][0] + (i * 6)
+            end = annotation[1][1] + (i * 6)
+
+            text = text[0:start] + annotation[0] + text[end:]
+
+            i+= 1
+
+        return text
