f704ec3ea5cadf6664bfb454d04dfe63f951d2c3
Fagner <pantoja.ti@gmail.com>
2020-10-31 11:41:05 -0300

    bert ner is working. Trying to achieve better results for fine-tuning

--- src/ner/bert/BertController.py
+++ src/ner/bert/BertController.py
@@ -1,11 +1,18 @@
 from .bert import Ner
+# from .biobert_pytorch import Ner
+
 # from src.ner.bert.Bert import Ner
 
 import os
 
 class BertController:
     def predict(self, text):
-        model = Ner("/app/src/ner/bert/biobert_out_base/")
+        # model = Ner("/app/src/ner/bert/pretrained_models/enzo_model/")
+        # model = Ner("/app/src/ner/bert/pretrained_models/biobert_pytorch")
+        model = Ner("/app/src/ner/bert/pretrained_models/BC5CDR-IOB")
+
+
+
         output = model.predict(text)
         print(output)
         return output
