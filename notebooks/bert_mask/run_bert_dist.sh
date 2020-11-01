6b38b350d31121f4446ff78e3fa2e728995ddde5
Fagner <pantoja.ti@gmail.com>
2020-11-01 16:02:13 -0300

    selecting notebooks to maintain

--- notebooks/bert_mask/run_bert_dist.sh
+++
@@ -1,3 +0,0 @@
-python dist.py bert-base-cased 0 bert_tokens.txt bert_vectors.txt 0
-#python dist.py bert-base-cased 0 bert_tokens.txt bert_vectors.txt 1
-
--- notebooks/bert_mask/run_robert_dist.sh
+++
@@ -1,1 +0,0 @@
-python dist.py roberta-base 0 robert_tokens.txt robert_vectors.txt 0
