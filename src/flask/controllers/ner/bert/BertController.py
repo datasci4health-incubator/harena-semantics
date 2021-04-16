1e27dcc462bd1c9ffa9363ea7c001e5bc64884c5
Fagner <pantoja.ti@gmail.com>
2021-04-16 02:08:22 -0300

    feature: multiclass Bio NER

--- src/flask/controllers/ner/bert/BertController.py
+++ src/flask/controllers/ner/bert/BertController.py
@@ -1,4 +1,4 @@
-from .bert import Ner
+from .bert_v2 import Ner
 # from .biobert_pytorch import Ner
 
 # from src.ner.bert.Bert import Ner
@@ -11,13 +11,34 @@ class BertController:
         # model = Ner("/app/src/ner/bert/pretrained_models/biobert_pytorch")
         # pretrained_model = "/app/src/ner/bert/pretrained_models/BC5CDR-IOB"
         # pretrained_model = "/app/src/ner/bert/pretrained_models/biobert_ner"
-        model = "/app/models/word_embeddings/BC5CDR-IOB"
-        # model = "/app/models/word_embeddings/biobert_base_NER_NCBI-disease"
+        # model = "/app/models/word_embeddings/BC5CDR-IOB"
+        model = "/app/models/word_embeddings/biobert/AnatEM-BC5CDR"
 
         # print('pretrained_model', pretrained_model)
         model = Ner(model)
 
 
         output = model.predict(text)
+        # print(output)
+        return output
+
+
+    # MODEL_DIR = "/app/models/word_embeddings/biobert/AnatEM-BC5CDR"
+    MODEL_DIR = "/app/models/word_embeddings/biobert/ACD"
+
+
+    def predict_v2(self, text):
+        # model = Ner("/app/src/ner/bert/pretrained_models/enzo_model/")
+        # model = Ner("/app/src/ner/bert/pretrained_models/biobert_pytorch")
+        # pretrained_model = "/app/src/ner/bert/pretrained_models/BC5CDR-IOB"
+        # pretrained_model = "/app/src/ner/bert/pretrained_models/biobert_ner"
+        # model = "/app/models/word_embeddings/BC5CDR-IOB"
+
+
+        # print('pretrained_model', pretrained_model)
+        model = Ner(self.MODEL_DIR)
+
+
+        output = model.predict_v2(text)
         # print(output)
         return output
